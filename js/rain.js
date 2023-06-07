407ac4c41f06e0f51596163a88dc4807aa49b917
Kokky02 <daniel.kokay@gmail.com>
2023-06-07 20:39:36 +0200

    napojení deště při zapnutí soft-rain.mp3

--- js/rain.js
+++ js/rain.js
@@ -1,36 +1,80 @@
-let rainButton = document.getElementById('showRain');
-let modal = document.getElementById('rainModal');
-let playButton = document.getElementById('playButton');
-let closeButton = document.getElementById('closeButton');
-let volumeSlider = document.getElementById('volumeSlider');
-let audioRain = new Audio('sounds/soft-rain.mp3');
-let isPlaying = false;
+$(document).ready(function() {
+  let rainButton = $('#showRain')
+  let modal = $('#rainModal')
+  let playButton = $('#playButton')
+  let closeButton = $('#closeButton')
+  let volumeSlider = $('#volumeSlider')
+  let audioRain = new Audio('sounds/soft-rain.mp3')
+  let isPlaying = false
+  let rainAnim = $('.rain')
 
-audioRain.loop = true; // Nastavení opakování přehrávání
+  audioRain.loop = true // Nastavení opakování přehrávání
 
-rainButton.addEventListener('click', function() {
-  modal.style.display = 'block';
-  rainButton.style.display = 'none'
-});
+  rainButton.on('click', function() {
+    modal.css('display', 'block')
+    rainButton.css('display', 'none')
+  })
 
-closeButton.addEventListener('click', function() {
-  modal.style.display = 'none';
-  rainButton.style.display = 'block'
-});
+  closeButton.on('click', function() {
+    modal.css('display', 'none')
+    rainButton.css('display', 'block')
+  })
 
-playButton.addEventListener('click', function() {
-  if (isPlaying) {
-    audioRain.pause();
-    isPlaying = false;
-    
-  } else {
-    audioRain.play();
-    isPlaying = true;
-    
+  playButton.on('click', function() {
+    if (isPlaying) {
+      audioRain.pause()
+      isPlaying = false
+      rainAnim.css('display', 'none')      
+    } else {
+      audioRain.play()
+      isPlaying = true
+      rainAnim.css('display', 'flex')
+    }
+  });
+
+  volumeSlider.on('input', function() {
+    let volume = parseFloat(volumeSlider.val()) / 100
+    audioRain.volume = volume
+  });
+
+  // rain Graphic
+
+  let makeItRain = function() {
+    $('.rain').empty()
+
+    let increment = 0
+    let drops = ""
+    let backDrops = ""
+
+    while (increment < 100) {
+      let randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1))
+      let randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2))
+      increment += randoFiver
+      drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+      backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+
+    $('.rain.front-row').append(drops)
+    $('.rain.back-row').append(backDrops)
   }
-});
 
-volumeSlider.addEventListener('input', function() {
-  let volume = parseFloat(volumeSlider.value) / 100;
-  audioRain.volume = volume;
+  $('.splat-toggle.toggle').on('click', function() {
+    $('body').toggleClass('splat-toggle')
+    $('.splat-toggle.toggle').toggleClass('active')
+    makeItRain()
+  })
+
+  $('.back-row-toggle.toggle').on('click', function() {
+    $('body').toggleClass('back-row-toggle')
+    $('.back-row-toggle.toggle').toggleClass('active')
+    makeItRain();
+  })
+
+  $('.single-toggle.toggle').on('click', function() {
+    $('body').toggleClass('single-toggle')
+    $('.single-toggle.toggle').toggleClass('active')
+    makeItRain()
+  })
+
+  makeItRain()
 });
